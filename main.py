e2c27852c6290249b46f1d897dda0b10b3b4e0f0
tyjch <tyjchurchill@gmail.com>
2025-03-21 18:56:01 -0700

    Added bias logging to main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import asyncio
 from pprint import pprint
 from dotenv import load_dotenv
 from loguru import logger
+from gpiozero import Device
+from gpiozero.pins.lgpio import LGPIOFactory
 from sensors.base import Sensor, Measurement
 from sensors.ds18b20 import DS18B20
 from sensors.sht41 import SHT41
@@ -15,36 +17,38 @@ from display.layers.temperature import TemperatureLayer
 from display.layers.wifi import WifiLayer
 from display.layers.menu import MenuLayer
 
+      
 load_dotenv()
 
+temp_layer = TemperatureLayer()
+wifi_layer = WifiLayer()
+menu_layer = MenuLayer()
+screen     = Screen(layers=[wifi_layer, menu_layer, temp_layer])
+
+buffer = MeasurementBuffer()
+influx = InfluxClient(
+  url    = os.getenv('INFLUX_URL'),
+  token  = os.getenv('INFLUX_TOKEN'),
+  org    = os.getenv('INFLUX_ORG'),
+  bucket = os.getenv('INFLUX_BUCKET'),
+  buffer = buffer
+)
+
+sampler = Sampler(
+  sensors    = [DS18B20()], #, SHT41(), RaspberryPi()],
+  dimensions = ['temperature', 'relative_humidity', 'cpu_load', 'cpu_temp']
+)
+
 async def main():
-  temp_layer = TemperatureLayer()
-  wifi_layer = WifiLayer()
-  menu_layer = MenuLayer()
-  screen     = Screen(layers=[temp_layer, wifi_layer, menu_layer])
-  
-  buffer = MeasurementBuffer()
-  influx = InfluxClient(
-    url    = os.getenv('INFLUX_URL'),
-    token  = os.getenv('INFLUX_TOKEN'),
-    org    = os.getenv('INFLUX_ORG'),
-    bucket = os.getenv('INFLUX_BUCKET'),
-    buffer = buffer
-  )
-  
-  sampler = Sampler(
-    sensors    = [DS18B20()], #, SHT41(), RaspberryPi()],
-    dimensions = ['temperature', 'relative_humidity', 'cpu_load', 'cpu_temp']
-  )
-  
   state = {
     'fahrenheit' : 0.0, 
     'bias'       : 0.0
   }
   
+  last_bias = 0.0
+  
   while True:
     measurements = await sampler.get_measurements()
-    
     for m in measurements:
       if m.sensor_name == 'DS18B20':
         state['fahrenheit'] = m.value
@@ -53,9 +57,13 @@ async def main():
         if new_state:
           state.update(new_state)
           logger.debug(f'State updated to: {state}')
-          
       try:
-        influx.insert_point(m)
+        influx.insert_measurement(m)
+        # Only log bias if it has changed
+        current_bias = state['bias']
+        if last_bias != current_bias:
+          influx.insert_bias(current_bias, m)
+          last_bias = current_bias
       except Exception as e:
         logger.error(e)
         raise e
@@ -63,3 +71,5 @@ async def main():
 
 if __name__ == '__main__':
   asyncio.run(main())
+  
+# Add atexit?
