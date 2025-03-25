941cc92dc61b1c2f6b9b7fa4ceeed96690b323b6
tyjch <tyjchurchill@gmail.com>
2025-03-25 16:20:39 -0700

    Modified imports

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import os
 import sys
 import asyncio
-from pprint import pprint
 from dotenv import load_dotenv
 from loguru import logger
 from gpiozero import Device
 from gpiozero.pins.lgpio import LGPIOFactory
-from sensors.base import Sensor, Measurement
-from sensors.ds18b20 import DS18B20
-from sensors.sht41 import SHT41
-from sensors.pi import RaspberryPi
+from sensors import Sensor, Measurement, DS18B20, SHT41, RaspberryPi
 from sampler import Sampler
-from clients.influx import InfluxClient
-from clients.buffer import MeasurementBuffer
-from display.screen import Screen
-from display.layers.temperature import TemperatureLayer
-from display.layers.wifi import WifiLayer
-from display.layers.menu import MenuLayer
+from clients import InfluxClient, MeasurementBuffer
+from display import Screen
+from display.layers import TemperatureLayer, WifiLayer, MenuLayer
 
-      
 load_dotenv()
 
+#region Logger
 def format_log(record):
   level    = record['level'].name
   time     = record['time']
@@ -37,26 +30,23 @@ def format_log(record):
 
   return f'\n{time_str} | {level_str} | {location_str} | {message_str}'
 
-def filter_log(record, allowed_tags):
-  record_tags = record['extra'].get('tags', [])
-  return any(tag in record_tags for tag in allowed_tags)
-
 logger.remove(0)
-logger.add(
-  sys.stderr, 
-  colorize = True, 
-  format   = format_log
-)
+logger.add(sys.stderr, colorize=True, format=format_log)
+#endregion
 
+#region Screen 
+screen = Screen()
 temp_layer = TemperatureLayer()
 wifi_layer = WifiLayer()
 menu_layer = MenuLayer()
-
-screen = Screen(layers=[
+screen.layers = [
+  temp_layer, 
   wifi_layer, 
-  menu_layer, 
-  temp_layer
-])
+  menu_layer
+]
+#endregion
+
+#region Measurements 
 
 buffer = MeasurementBuffer()
 influx = InfluxClient(
@@ -71,9 +61,22 @@ sampler = Sampler(
   sensors    = [DS18B20(), SHT41(), RaspberryPi()],
   dimensions = ['temperature', 'relative_humidity', 'cpu_load', 'cpu_temp']
 )
+#endregion
 
 
-async def poll_sensors(state, sampler, influx, sleep_seconds=1):
+async def poll_sensors(state, sleep_seconds=1):
+  buffer  = MeasurementBuffer()
+  influx  = InfluxClient(
+    url    = os.getenv('INFLUX_URL'),
+    token  = os.getenv('INFLUX_TOKEN'),
+    org    = os.getenv('INFLUX_ORG'),
+    bucket = os.getenv('INFLUX_BUCKET'),
+    buffer = buffer
+  )
+  sampler = Sampler(
+    sensors    = [DS18B20(), SHT41(), RaspberryPi()],
+    dimensions = ['temperature', 'relative_humidity', 'cpu_load', 'cpu_temp']
+  )
   while True:
     measurements = await sampler.get_measurements()
     for m in measurements:
@@ -83,7 +86,6 @@ async def poll_sensors(state, sampler, influx, sleep_seconds=1):
         influx.insert_measurement(m)
         current_bias = state['bias']
         if state['last_bias'] != current_bias: 
-          # Only send bias if it has changed
           influx.insert_bias(current_bias, m)
           state['last_bias'] = current_bias
       except Exception as e:
@@ -103,10 +105,11 @@ async def main():
     'fahrenheit' : 0.0, 
     'bias'       : 0.0,
     'last_bias'  : 0.0,
-    'shutdown'   : False
+    'shutdown'   : False,
+    'location'   : 'main'
   }
   
-  sensor_task = asyncio.create_task(poll_sensors(state, sampler, influx))
+  sensor_task = asyncio.create_task(poll_sensors(state))
   screen_task = asyncio.create_task(refresh_screen(state, screen))
   
   await asyncio.gather(sensor_task, screen_task)
@@ -115,4 +118,16 @@ async def main():
 if __name__ == '__main__':
   asyncio.run(main())
   
-# Add atexit?
+'''
+Menu Navigation
+main -> [x] [x] [x] [menu]
+menu -> [shutoff] [cycle_wifi] [open_bias] [main]
+bias -> [x] [decrease_bias] [increase_bias] [main]
+'''
+
+'''
+Layer Updates
+temp_layer -> when temperature changes
+wifi_layer -> when connection strength, network, or ip changes
+menu_layer -> when button is pressed
+'''
