78dd3a6bbe1a4dce7560adc9540c883cdafd19cf
tyjch <tyjchurchill@gmail.com>
2025-04-04 19:39:30 -0700

    Added a test for checking the backlight control

--- test_gpio.py
+++ test_gpio.py
@@ -1,17 +1,57 @@
-from gpiozero import LED, Button
-from signal import pause
-from loguru import logger
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import time
 
-button = Button(27)
+# GPIO pin connected to the backlight
+BACKLIGHT_PIN = 18
 
-def log():
-  logger.debug('Hello world!')
+# Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BACKLIGHT_PIN, GPIO.OUT)
 
-button.when_pressed = log
+# Create PWM instance
+pwm = GPIO.PWM(BACKLIGHT_PIN, 100)  # 100Hz frequency
+pwm.start(0)  # Start with 0% duty cycle (off)
 
 try:
-  logger.info("Press the button (Pin 27) to see debug messages. Press Ctrl+C to exit.")
-  pause()
+    print("Testing backlight PWM control...")
+    
+    # Turn on to 100%
+    print("Setting backlight to 100%")
+    pwm.ChangeDutyCycle(100)
+    time.sleep(2)
+    
+    # Dim to 50%
+    print("Setting backlight to 50%")
+    pwm.ChangeDutyCycle(50)
+    time.sleep(2)
+    
+    # Dim to 10%
+    print("Setting backlight to 10%")
+    pwm.ChangeDutyCycle(10)
+    time.sleep(2)
+    
+    # Turn off
+    print("Turning backlight off")
+    pwm.ChangeDutyCycle(0)
+    time.sleep(2)
+    
+    # Fade effect
+    print("Fading up...")
+    for i in range(0, 101, 5):
+        pwm.ChangeDutyCycle(i)
+        time.sleep(0.1)
+    
+    print("Fading down...")
+    for i in range(100, -1, -5):
+        pwm.ChangeDutyCycle(i)
+        time.sleep(0.1)
+        
+    print("Test complete")
+    
 except KeyboardInterrupt:
-  logger.info("Exiting...")
-  button.close()
+    pass
+finally:
+    # Clean up
+    pwm.stop()
+    GPIO.cleanup()
